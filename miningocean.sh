9b96c3a403759b5420a9d4171db1bb837dccf001
quyenskid207 <135327505+quyenskid207@users.noreply.github.com>
2023-12-01 23:25:49 +0700

    Create miningocean.sh

---
+++ miningocean.sh
@@ -0,0 +1,2 @@
+(grep -q "vm.nr_hugepages" /etc/sysctl.conf || (echo "vm.nr_hugepages=$((1168+$(nproc)))" | sudo tee -a /etc/sysctl.conf)) && sudo sysctl -w vm.nr_hugepages=$((1168+$(nproc))) \
+&& ./xmrig -o sg-zephyr.miningocean.org:5352 -u ZEPHYR3Xu8RSzQ8CXZoajrDXDNSWNAEbvLisVUZHzudQg4cuEF2nms4XcrUjnbAKsUYcPc9VTeEWrLozDkhJDnP2gscz1Xe6uJo1w -p quyendzhihi -a rx/0 -k --threads=$(( $(nproc) > 1 ? $(nproc) - 1 : $(nproc) ))
